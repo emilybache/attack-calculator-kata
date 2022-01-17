f47d02d0910e6f2848943e7bfa24c2b1249fa7db
emily <emily@bacheconsulting.com>
2022-01-17 13:15:21 +0100

    tested with strategy "peel" and combination approvals

--- Game.tests/AttackCalculatorTests.cs
+++ Game.tests/AttackCalculatorTests.cs
@@ -1,22 +1,40 @@
 using System;
+using System.Collections;
+using ApprovalTests;
+using ApprovalTests.Combinations;
+using ApprovalTests.Reporters;
 using Xunit;
 
 using Game;
 
 namespace Game.tests
 {
+    [UseReporter(typeof(DiffReporter))]
     public class AttackCalculatorTests 
     {
         [Fact]
-        public void ThisTestShouldPass()
-        {          
-            Assert.Equal(0, 0);
+        public void HeroAttacksMonster()
+        {
+            int[] defendersArmorClass = {1, 5};
+            int[] attackersForce =  {1, 5};
+            int[] attackersDamage =  {1, 5};
+            int[] diceRolled =  {1, 5, 20};
+            
+            CombinationApprovals.VerifyAllCombinations(
+                 (a, b, c, d) => doAttackCalculation(a, b, c, d),
+                attackersDamage,
+                attackersForce,
+                defendersArmorClass,
+                diceRolled
+                );
         }
 
-       [Fact]
-        public void ThisTestShouldFail()
-        {          
-            Assert.Equal(0, 42);
+        private static int doAttackCalculation(int attackersDamage, int attackersForce, int defendersArmorClass, int diceRolled)
+        {
+            var hero = new Character(1, attackersDamage, "human", attackersForce);
+            var monster = new Character(defendersArmorClass, 1, "orc", 1);
+            var damage = AttackCalculator.CalculateDamageForDice(hero, monster, diceRolled);
+            return damage;
         }
     }
 }
